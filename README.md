b3b10c6dcbd50038775da798affb4cf3bbaf8341
Parbati <paru@Parbatis-MacBook-Pro.local>
2022-10-14 15:18:04 +1100

    initial change

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# commerce-admin-frontend
+ # Ecommerce admin cms
+
+This project is for the admin panel of the ecommerce site.
+
+## How to run
+- Clone the project
+- Run `npm install`
---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Starting Admin CMS
+
+      <Button variant='success'>test button</Button>
+      
+    </div>
+  );
+}
+
+export default App;
